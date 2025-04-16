873c4845a9e7aa3bc659184b6d22582c45727702
fredrik-hjarner <35084243+fredrik-hjarner@users.noreply.github.com>
2025-04-16 22:28:59 +0200

    fasmg m69k stuff.

--- src/fasmg/m68k/test.asm
+++ src/fasmg/m68k/test.asm
@@ -5,7 +5,6 @@ include "m68k.inc"
 ;; All niladic instructions.                                                  ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-; db "illegal", 0
 ; TODO: Every instruction must start on a word-aligned address.
 illegal
 nop
@@ -18,6 +17,26 @@ trapv
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; All monadic instructions.                                                  ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+ext.w d0
+ext.w d1
+ext.w d2
+ext.w d3
+ext.w d4
+ext.w d5
+ext.w d6
+ext.w d7
+ext.w 8 ; TODO: This should generate error because only d0-d7 are allowed.
+
+ext.l d0
+ext.l d1
+ext.l d2
+ext.l d3
+ext.l d4
+ext.l d5
+ext.l d6
+ext.l d7
+ext.l 8 ; TODO: This should generate error because only d0-d7 are allowed.
 
 swap d0
 swap d1
@@ -35,3 +54,22 @@ trap 1
 trap 14
 trap 15
 ; trap 16 ; TODO: THis should generate error. 15 is highest allowed.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; All dyadic instructions.                                                   ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+moveq 0, d0
+moveq 0, d1
+moveq 0, d2
+moveq 0, d3
+moveq 0, d4
+moveq 0, d5
+moveq 0, d6
+moveq 0, d7
+
+moveq 0, d0
+moveq 1, d0
+moveq 2, d0
+moveq 3, d0
+
