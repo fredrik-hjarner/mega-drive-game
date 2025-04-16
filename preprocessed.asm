7b2848dbe5df1b583d5e10dead8662997ec174e2
fredrik-hjarner <35084243+fredrik-hjarner@users.noreply.github.com>
2025-04-16 15:50:15 +0200

    cleanup fasmg m69k stuff.

--- preprocessed.asm
+++ preprocessed.asm
@@ -124,7 +124,7 @@ gamepads_get_input macro
     dc.b '                '
     dc.b 'EPIC LEGENDS OF DESTINY                         '
     dc.b 'EPIC LEGENDS OF DESTINY                         '
-    dc.b 'GM 152047 '
+    dc.b 'GM 152090 '
     org $18E
     dc.w $0000
     dc.b 'J               '
